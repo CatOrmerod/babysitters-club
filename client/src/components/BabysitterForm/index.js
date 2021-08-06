ef5e055794ebb31125ac80d713072321079fbc4a
Cat Ormerod <catormerod@gmail.com>
2021-08-06 19:28:16 +1000

    Added image upload function for babysitter form

--- client/src/components/BabysitterForm/index.js
+++ client/src/components/BabysitterForm/index.js
@@ -1,12 +1,22 @@
 import React, { useState } from 'react';
-import { Button, Form, Message } from 'semantic-ui-react';
+// import { Button, Form, Message } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
 import { ADD_BABYSITTER } from '../../utils/mutations';
 import { QUERY_BABYSITTERS, QUERY_ME } from '../../utils/queries';
 import Auth from '../../utils/auth';
-// import ImageUpload from '../ImageUpload';
+import ImageUpload from '../ImageUpload';
 import Location from '../Location';
+import {
+  Button,
+  FormGroup,
+  Form,
+  Input,
+  Container,
+  Row,
+  Col,
+  CustomInput,
+} from "reactstrap";
 
 const BabysitterForm = () => {
   const [formState, setFormState] = useState({
@@ -31,11 +41,11 @@ const BabysitterForm = () => {
       }
 
       // update me object's cache
-      // const { me } = cache.readQuery({ query: QUERY_ME });
-      // cache.writeQuery({
-      //   query: QUERY_ME,
-      //   data: { me: { ...me, babysitters: [...me.babysitters, addBabysitter] } },
-      // });
+      const { me } = cache.readQuery({ query: QUERY_ME });
+      cache.writeQuery({
+        query: QUERY_ME,
+        data: { me: { ...me, babysitters: [...me.babysitters, addBabysitter] } },
+      });
     },
   });
 
@@ -69,87 +79,193 @@ const BabysitterForm = () => {
       [name]: value,
     });
   };
-
+console.log(formState)
   return (
     <>
       <h3>Details of your profile</h3>
 
       {Auth.loggedIn() ? (
+        <Container>
+          {/* <Row>
+            <div className="profile-picture">
+              <div
+                className="fileinput fileinput-new"
+                data-provides="fileinput"
+              >
+                <div className="fileinput-new img-no-padding">
+                  <img
+                    alt="..."
+                    src={
+                      require("/Users/catormerod/OneDrive/Coding Bootcamp/projects/babysitter/client/src/assets/images/beach-fun.jpeg").default
+                    }
+                  />
+                </div>
+                <div className="fileinput-preview fileinput-exists img-no-padding" />
+                <div>
+                  <Button
+                    className="btn-file btn-round"
+                    color="default"
+                    outline
+                  >
+                    <span className="fileinput-new">Change Photo</span>
+                    <span className="fileinput-exists">Change</span>
+                    <input name="..." type="file" />
+                  </Button>
+                  <br />
+                  <Button
+                    className="btn-link fileinput-exists"
+                    color="danger"
+                    data-dismiss="fileinput"
+                    href="#pablo"
+                    onClick={(e) => e.preventDefault()}
+                  >
+                    <i className="fa fa-times" />
+                    Remove
+                  </Button>
+                </div>
+              </div>
+            </div>
+          </Row> */}
+          <Row>
+            <Col className="ml-auto mr-auto" md="6">
+              <Form className='babysitter-form' onSubmit={handleFormSubmit}>
+                <Row>
+                  <Col md="3" sm="4">
+                  <div className="profile-picture">
+                    <h4 className="title">
+                    <small>Avatar</small>
+                    </h4>
+                    <ImageUpload avatar 
+                    setFormState={setFormState}/>
+                    </div>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col md="6" sm="6">
+                    <FormGroup>
+                      <label htmlFor='form-ph'>Phone Number:</label>
+                      <Input
+                      name="babysitterPh"
+                      placeholder="Enter your phone number (it won't be displayed)"
+                      onChange={handleChange}
+                      />
+                    </FormGroup>
+                  </Col>
+                  <Col md="6" sm="6">
+                    <FormGroup>
+                    <label htmlFor='form-cert'>Certificates</label>
+                <Input
+                  name="babysitterCert"
+                  placeholder="What certifications do you have?"
+                  onChange={handleChange}
+                />
+                    </FormGroup>
+                  </Col>
+                </Row>
+                <FormGroup>
+                  <label htmlFor='form-location'>Location</label>
+                  <Location
+                    handleChange={handleChange}
+                    formState={formState}
+                    setFormState={setFormState} />
+                </FormGroup>
+                <FormGroup>
+                  <label htmlFor='form-about'>About</label>
+                  <Input
+                    name="babysitterAbout"
+                    placeholder="Tell us all about yourself."
+                    className="textarea"
+                    type="textarea"
+                    maxLength="1256"
+                    rows="3"
+                    onChange={handleChange}
+                  />
+                  <h5>
+                    <small>
+                      <span
+                        className="pull-right"
+                        id="textarea-limited-message"
+                      >
+                        150 characters left
+                      </span>
+                    </small>
+                  </h5>
+                </FormGroup>
 
-        <Form className='menu-item-form' onSubmit={handleFormSubmit}>
-          <Form.Group widths='equal'>
-            <Form.Field>
-              <label htmlFor='form-about'>About</label>
-              <input
-                name="babysitterAbout"
-                placeholder="Tell us all about yourself"
-                className="form-input w-100"
-                style={{ lineHeight: '1.5', resize: 'vertical' }}
-                onChange={handleChange}
-              />
-            </Form.Field>
-            <Form.Field>
-              <label htmlFor='form-location'>Location</label>
+                <div className="text-center">
+                  <Button
+                    className="btn-wd btn-round"
+                    color="info"
+                    type="submit"
+                  >
+                    Save
+                  </Button>
+                </div>
+              </Form>
+            </Col>
+          </Row>
+          {/* <Form className='babysitter-form' onSubmit={handleFormSubmit}>
+            <Form.Group widths='equal'>
+              <Form.Field>
+                <label htmlFor='form-about'>About</label>
+                <input
+                  name="babysitterAbout"
+                  placeholder="Tell us all about yourself"
+                  className="form-input w-100"
+                  style={{ lineHeight: '1.5', resize: 'vertical' }}
+                  onChange={handleChange}
+                />
+              </Form.Field>
+              <Form.Field>
+                <label htmlFor='form-location'>Location</label>
 
-              <Location 
-              handleChange={handleChange}
-              formState={formState}
-              setFormState={setFormState}/>
-            </Form.Field>
-            
-            {/* <Form.Field>
-              <label htmlFor='form-loc'>Postcode</label>
-              <input
-                name="babysitterLoc"
-                placeholder="Enter your postcode"
-                className="form-input w-100"
-                style={{ lineHeight: '1.5', resize: 'vertical' }}
-                onChange={handleChange}
-              />
-            </Form.Field> */}
-
-            <Form.Field>
-              <label htmlFor='form-cert'>Certificates</label>
-              <input
-                name="babysitterCert"
-                placeholder="What certifications do you have?"
-                className="form-input w-100"
-                style={{ lineHeight: '1.5', resize: 'vertical' }}
-                onChange={handleChange}
-              />
-            </Form.Field>
-            <Form.Field>
-              <label htmlFor='form-image-upload'>Image</label>
-              {/* <ImageUpload /> */}
-            </Form.Field>
-            <Form.Field>
-              <label htmlFor='form-ph'>Phone Number:</label>
-              <input
-                name="babysitterPh"
-                placeholder="Enter your phone number (it won't be displayed)"
-                className="form-input w-100"
-                style={{ lineHeight: '1.5', resize: 'vertical' }}
-                onChange={handleChange}
-              />
-            </Form.Field>
+                <Location
+                  handleChange={handleChange}
+                  formState={formState}
+                  setFormState={setFormState} />
+              </Form.Field>
+              <Form.Field>
+                <label htmlFor='form-cert'>Certificates</label>
+                <input
+                  name="babysitterCert"
+                  placeholder="What certifications do you have?"
+                  className="form-input w-100"
+                  style={{ lineHeight: '1.5', resize: 'vertical' }}
+                  onChange={handleChange}
+                />
+              </Form.Field>
+              <Form.Field>
+                <label htmlFor='form-image-upload'>Image</label>
+                <ImageUpload />
+              </Form.Field>
+              <Form.Field>
+                <label htmlFor='form-ph'>Phone Number:</label>
+                <input
+                  name="babysitterPh"
+                  placeholder="Enter your phone number (it won't be displayed)"
+                  className="form-input w-100"
+                  style={{ lineHeight: '1.5', resize: 'vertical' }}
+                  onChange={handleChange}
+                />
+              </Form.Field>
             </Form.Group>
 
-            <Button type='submit'>Save</Button>
+            <Button type='submit'>Save</Button> */}
 
             {error && (
               <div className="col-12 my-3 bg-danger text-white p-3">
                 {error.message}
               </div>
             )}
-          </Form>
-          ) : (
-          <p>
-            You need to be logged in to add yourself to our database. Please{' '}
-            <Link to="/login">login</Link> or <Link to="/signup">signup.</Link>
-          </p>
-          )}  
-          </>
+        </Container>
+      ) : (
+        <p>
+          You need to be logged in to add yourself to our database. Please{' '}
+          <Link to="/login">login</Link> or <Link to="/signup">signup.</Link>
+        </p>
+      )}
+    </>
 
-      );
+  );
 };
-      export default BabysitterForm;
+export default BabysitterForm;
--- client/src/components/ImageUpload/index.js
+++ client/src/components/ImageUpload/index.js
@@ -1,40 +1,95 @@
-import React from 'react';
-import firebase from 'firebase';
-import FileUploader from 'react-firebase-file-uploader';
+import React from "react";
+// used for making the prop types of this component
+import PropTypes from "prop-types";
 
-import { UPDATE_BABYSITTER_PHOTO } from '../../utils/actions';
-import { useStoreContext } from '../../utils/GlobalState';
+import { Button } from "reactstrap";
 
-const ImageUpload = () => {
-    const [state, dispatch] = useStoreContext();
+import defaultImage from "/Users/catormerod/OneDrive/Coding Bootcamp/projects/babysitter/client/src/assets/images/image_placeholder.jpg";
+import defaultAvatar from "/Users/catormerod/OneDrive/Coding Bootcamp/projects/babysitter/client/src/assets/images/placeholder.jpg";
 
-    const handleUploadError = error =>  console.log(error);
+function ImageUpload(props) {
+  const [file, setFile] = React.useState();
+  const [imagePreviewUrl, setImagePreviewUrl] = React.useState(
+    props.avatar ? defaultAvatar : defaultImage
+  );
+  const fileInput = React.useRef(null);
+  const handleImageChange = (e) => {
+    e.preventDefault();
+    if (e.target.files[0]) {
+      let reader = new FileReader();
+      let file = e.target.files[0];
+      reader.onloadend = () => {
+        props.setFormState( (formState) => {
+          return {
+            ...formState,
+            babysitterPic: reader.result
+          }
+        });
+        console.log(reader.result)
+        setFile(reader.result)
+        setImagePreviewUrl(reader.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+  // const handleSubmit = e => {
+  // e.preventDefault();
+  // this.state.file is the file/image uploaded
+  // in this function you can save the image (this.state.file) on form submit
+  // you have to call it yourself
+  // };
+  const handleClick = () => {
+    fileInput.current.click();
+  };
+  const handleRemove = () => {
+    props.setFormState(null);
+    setImagePreviewUrl(props.avatar ? defaultAvatar : defaultImage);
+    fileInput.current.value = null;
+  };
+  return (
+    <div className="fileinput text-center">
+      <input type="file" onChange={handleImageChange} ref={fileInput} />
+      <div className={"thumbnail" + (props.avatar ? " img-circle" : "")}>
+        <img src={imagePreviewUrl} alt="..." />
+      </div>
+      <div>
+        {file === null ? (
+          <Button
+            className="btn-round"
+            color="default"
+            outline
+            onClick={handleClick}
+          >
+            {props.avatar ? "Add Photo" : "Select image"}
+          </Button>
+        ) : (
+          <span>
+            <Button
+              className="btn-round"
+              outline
+              color="default"
+              onClick={handleClick}
+            >
+              Change
+            </Button>
+            {props.avatar ? <br /> : null}
+            <Button
+              color="danger"
+              className="btn-round btn-link"
+              onClick={handleRemove}
+            >
+              <i className="fa fa-times" />
+              Remove
+            </Button>
+          </span>
+        )}
+      </div>
+    </div>
+  );
+}
 
-    const handleUploadSuccess = filename => {
-
-        firebase
-            .storage()
-            .ref('images')
-            .child(filename)
-            .getDownloadURL()
-            .then(url => dispatch({
-                type: UPDATE_BABYSITTER_PHOTO,
-                itemPreview: {
-                    ...state.itemPreview,
-                    image: url
-                }
-            }));
-    };
-
-    return (
-        <FileUploader
-            accept='image/*'
-            name='image'
-            storageRef={firebase.storage().ref('images')}
-            onUploadError={handleUploadError}
-            onUploadSuccess={handleUploadSuccess}
-        />
-    );
+ImageUpload.propTypes = {
+  avatar: PropTypes.bool,
 };
 
 export default ImageUpload;
