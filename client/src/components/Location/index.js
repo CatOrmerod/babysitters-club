84bbb13866be2516791cb79b0282b2862892fd14
Cat Ormerod <catormerod@gmail.com>
2021-08-09 19:07:22 +1000

    change back to http

--- client/src/components/Location/index.js
+++ client/src/components/Location/index.js
@@ -16,7 +16,7 @@ const Location = ({ handleChange, formState, setFormState }) => {
   }
   // calling API -- searchTerm as a parameter 
   const searchPostcode = async (searchTerm) => {
-    const url = `https://api.geonames.org/postalCodeSearchJSON?postalcode=${searchTerm}&maxRows=10&username=catormerod`;
+    const url = `http://api.geonames.org/postalCodeSearchJSON?postalcode=${searchTerm}&maxRows=15&username=catormerod`;
 
     const response = await fetch(url);
     const responseJson = await response.json();
